1e21f6df7f2ea8130511cdb027fc1c7d701896fc
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-09-15 03:33:06 +0200

    Lagt til kommentar

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+/**
+ * Oppgave 15 b) 
+ * 
+ * Lag et program hvor det gjelder å trykke så fort som mulig på A-knappen Vis ikoner på skjermen i forhold til antall trykk
+ */
 basic.forever(function () {
 	
 })
